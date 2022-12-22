de98434ed82780cf083b60ab02723b6564c532e1
Emona Santiago <emonasantiago1207@gmail.com>
2022-12-22 13:55:50 +0100

    header route

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,7 @@
 <script>
   import "./+page.svelte";
   import "./../styles/global.css";
+  import Header from "./Header.svelte";
 </script>
+
+<Header />
---
+++ src/routes/Header.svelte
@@ -0,0 +1,1 @@
+<h2>IK BEN DE HEADER!</h2>
